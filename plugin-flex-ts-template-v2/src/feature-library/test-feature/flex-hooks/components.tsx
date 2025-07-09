6cfb15915239130f05343f6f3ee144b7c51f6bd5
pheathtwilio <pheath@twilio.com>
2025-07-09 15:50:07 -0500

    removed logging from the test feature

--- plugin-flex-ts-template-v2/src/feature-library/test-feature/flex-hooks/components.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/test-feature/flex-hooks/components.tsx
@@ -3,6 +3,5 @@ import * as Flex from '@twilio/flex-ui';
 import TestFeatureComponent from '../custom-components/TestFeatureComponent';
 
 export const componentHook = function addTestFeatureComponentToPanel(flex: typeof Flex) {
-  console.log('[test-feature] componentHook running');
   flex.AgentDesktopView.Panel2.Content.replace(<TestFeatureComponent key="test-feature" />, { sortOrder: 999 });
 };
--- plugin-flex-ts-template-v2/src/feature-library/test-feature/index.ts
+++ plugin-flex-ts-template-v2/src/feature-library/test-feature/index.ts
@@ -4,10 +4,8 @@ import { hooks } from './flex-hooks';
 
 export const register = (): FeatureDefinition => {
   if (!isFeatureEnabled()) {
-    console.log('[test-feature] disabled by config');
     return {};
   }
-  console.log('[test-feature] enabled! hooks:', hooks);
   return {
     name: 'test-feature',
     hooks,
